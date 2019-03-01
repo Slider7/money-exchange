14915ec0dc89e150debd0a2f38c0968523a0a4d9
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-03-01 17:49:49 +0600

    fix: fix result object / finish task

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ module.exports = function makeExchange(currency) {
         mon = [50, 25, 10, 5, 1]; //номиналы монет H=50, Q=25, D=10, N=5, P=1
     let resObj = {"H":0,"Q":0,"D":0,"N":0,"P":0};
 
-      for (let i = 0; i < 5; i++){
-        if (i > 0) {d = 0};
-        resObj[Object.keys(resObj)[i]] = d * 2 + Math.trunc(c / mon[i]);
-        c = c % mon[i];
+    for (let i = 0; i < 5; i++){
+      if (i > 0) {d = 0}; //количество долларов будем умножать на 2 только для половинок
+       resObj[Object.keys(resObj)[i]] = d * 2 + Math.trunc(c / mon[i]);
+       c = c % mon[i];
+    }
+    
+    let newObj = {};
+    //добавим непустые поля из resObj в newObj
+    Object.keys(resObj).forEach(prop => {
+      if (resObj[prop]) {
+        newObj[prop] = resObj[prop]; 
       }
+    });
 
-    return resObj;
+    return newObj;
 
   }
