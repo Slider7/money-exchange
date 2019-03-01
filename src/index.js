1b46284276b9f1648bc49244497c111c97163be0
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-03-01 17:19:12 +0600

    fix: add condition and main loop to function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) return {};
+    if (currency > 10000 ) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    
+    let c = currency % 100, //монеты
+        d = currency - c, //доллары
+        mon = [50, 25, 10, 5, 1]; //номиналы монет H=50, Q=25, D=10, N=5, P=1
+    let resObj = {"H":0,"Q":0,"D":0,"N":0,"P":0};
+
+      for (let i = 0; i < 5; i++){
+        resObj[Object.keys(resObj)[i]] = Math.trunc(c / mon[i]);
+        c = c % mon[i];
+      }
+    
+      return resObj;
+
 }
