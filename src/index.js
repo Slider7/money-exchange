0a64f3aecad03faf98dde4a0d626caa270c2c0f2
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-03-01 17:33:08 +0600

    fix: correct main loop. need to fix result object

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ module.exports = function makeExchange(currency) {
     if (currency > 10000 ) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
     
     let c = currency % 100, //монеты
-        d = currency - c, //доллары
+        d = Math.trunc(currency / 100), //d- количество долларов
         mon = [50, 25, 10, 5, 1]; //номиналы монет H=50, Q=25, D=10, N=5, P=1
     let resObj = {"H":0,"Q":0,"D":0,"N":0,"P":0};
 
       for (let i = 0; i < 5; i++){
-        resObj[Object.keys(resObj)[i]] = Math.trunc(c / mon[i]);
+        if (i > 0) {d = 0};
+        resObj[Object.keys(resObj)[i]] = d * 2 + Math.trunc(c / mon[i]);
         c = c % mon[i];
       }
-    
-      return resObj;
 
-}
+    return resObj;
+
+  }
